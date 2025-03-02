b3b8309965fdaa4f969770520003acdb40de8294
Shafayet Fahim <s.fahim@rochester.edu>
2025-03-02 01:40:25 -0500

    Finalized main.py
    Needs revision from Riz.

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+"""
+CSC 242 Project 2: Source Code
+Authors: Rizouana Prome, Shafayet Fahim
+Submission Date: March 3rd, 2025
+Description: This .py file serves as the source code for a Sudoku solver.
+Note: To run our program, use "python sudoku_solver.py <test_case.txt>"; it'll
+detect the .txt file as the second argument (argv[1]).
+"""
+
 from collections import deque
 import copy
 import sys
 
-def print_grid(sudoku_grid):
-    for row in sudoku_grid:
+def print_board(sudoku_board):
+    for row in sudoku_board:
         print(" ".join(map(str, row)))
-def initialize_domains(sudoku_grid):
+def initialize_domains(sudoku_board):
     domains = {}
     for row in range(9):
         for column in range(9):
-            if sudoku_grid[row][column] != 0: domains[(row, column)] = [sudoku_grid[row][column]]
+            if sudoku_board[row][column] != 0: domains[(row, column)] = [sudoku_board[row][column]]
             else:
                 possible = list(range(1, 10))
                 for i in range(9):
-                    if sudoku_grid[row][i] in possible: possible.remove(sudoku_grid[row][i])
-                    if sudoku_grid[i][column] in possible: possible.remove(sudoku_grid[i][column])
-                box_row, box_column = 3 * (row // 3), 3 * (column // 3)
+                    if sudoku_board[row][i] in possible: possible.remove(sudoku_board[row][i])
+                    if sudoku_board[i][column] in possible: possible.remove(sudoku_board[i][column])
+                subgrid_row, subgrid_column = 3 * (row // 3), 3 * (column // 3)
                 for i in range(3):
                     for j in range(3):
-                        value = sudoku_grid[box_row + i][box_column + j]
+                        value = sudoku_board[subgrid_row + i][subgrid_column + j]
                         if value in possible: possible.remove(value)
                 domains[(row, column)] = possible
     return domains
@@ -63,82 +72,74 @@ def revise(domains, target_cell, neighbor_cell):
             domains[target_cell].remove(target_value)
             domain_changed = True
     return domain_changed
-def fill_single_candidates(sudoku_grid, domains, neighbors):
+def fill_single_candidates(sudoku_board, domains, neighbors):
     while True:
         updated = False
         for (row, column), candidates in list(domains.items()):
-            if sudoku_grid[row][column] == 0 and len(candidates) == 1:
+            if sudoku_board[row][column] == 0 and len(candidates) == 1:
                 value = candidates[0]
-                sudoku_grid[row][column] = value
-                for n_row, n_col in neighbors[(row, column)]:
-                    if value in domains[(n_row, n_col)]:
-                        domains[(n_row, n_col)].remove(value)
-                        if not domains[(n_row, n_col)]: return False
+                sudoku_board[row][column] = value
+                for neighbor_row, neighbor_column in neighbors[(row, column)]:
+                    if value in domains[(neighbor_row, neighbor_column)]:
+                        domains[(neighbor_row, neighbor_column)].remove(value)
+                        if not domains[(neighbor_row, neighbor_column)]: return False
                 updated = True
         if not updated: break
         result = ac3(domains, neighbors)
         if not result: return False
     return True
-
-def select_unassigned_variable(sudoku_grid, domains, neighbors):
-    min_size = 10
+def select_unassigned_variable(sudoku_board, domains, neighbors):
+    possible_values_count = 10
     chosen_cell = None
-    max_degree = -1
+    unassigned_neighbors = -1
     for row in range(9):
         for column in range(9):
-            if sudoku_grid[row][column] == 0:
+            if sudoku_board[row][column] == 0:
                 domain_size = len(domains[(row, column)])
                 degree = 0
-                for (n_row, n_column) in neighbors[(row, column)]:
-                    if sudoku_grid[n_row][n_column] == 0: degree += 1
-                if domain_size < min_size or (domain_size == min_size and degree > max_degree):
-                    min_size = domain_size
-                    max_degree = degree
+                for (neighbor_row, neighbor_column) in neighbors[(row, column)]:
+                    if sudoku_board[neighbor_row][neighbor_column] == 0: degree += 1
+                if domain_size < possible_values_count or (domain_size == possible_values_count and degree > unassigned_neighbors):
+                    possible_values_count = domain_size
+                    unassigned_neighbors = degree
                     chosen_cell = (row, column)
     return chosen_cell
-
 def order_domain_values(cell, domains, neighbors):
-    def sort_key(value):
+    conflict_counts = {}
+    for value in domains[cell]:
         count = 0
         for neighbor in neighbors[cell]:
             if value in domains[neighbor]: count += 1
-        return count
-    return sorted(domains[cell], key=sort_key)
-
-
-def backtracking_search(sudoku_grid, domains, neighbors):
-    if all(sudoku_grid[row][col] != 0 for row in range(9) for col in range(9)): return True
-    cell = select_unassigned_variable(sudoku_grid, domains, neighbors)
+        conflict_counts[value] = count
+    return sorted(domains[cell], key=conflict_counts.get)
+def backtracking_search(sudoku_board, domains, neighbors):
+    if all(sudoku_board[row][column] != 0 for row in range(9) for column in range(9)): return True
+    cell = select_unassigned_variable(sudoku_board, domains, neighbors)
     if cell is None: return False
-    row, col = cell
+    row, column = cell
     for value in order_domain_values(cell, domains, neighbors):
-        if value in domains[(row, col)]:
-            sudoku_grid[row][col] = value
+        if value in domains[(row, column)]:
+            sudoku_board[row][column] = value
             new_domains = copy.deepcopy(domains)
-            new_domains[(row, col)] = [value]
-            if ac3(new_domains, neighbors) and backtracking_search(sudoku_grid, new_domains, neighbors): return True
-            sudoku_grid[row][col] = 0
-
+            new_domains[(row, column)] = [value]
+            if ac3(new_domains, neighbors) and backtracking_search(sudoku_board, new_domains, neighbors): return True
+            sudoku_board[row][column] = 0
     return False
-
-def solve_sudoku(sudoku_grid):
-    domains = initialize_domains(sudoku_grid)
+def solve_sudoku(sudoku_board):
+    domains = initialize_domains(sudoku_board)
     neighbors = get_neighbors()
     if not ac3(domains, neighbors):
         print("No solution.")
         return
-    if not fill_single_candidates(sudoku_grid, domains, neighbors):
+    if not fill_single_candidates(sudoku_board, domains, neighbors):
         print("No solution.")
         return
-    if backtracking_search(sudoku_grid, domains, neighbors): print_grid(sudoku_grid)
+    if backtracking_search(sudoku_board, domains, neighbors): print_board(sudoku_board)
     else:
         print("No solution.")
+def read_sudoku_board(filename):
+    with open(filename, 'r') as test_case:
+        return [list(map(int, line.split())) for line in test_case if line.strip()]
 
-if __name__ == '__main__':
-    try:
-        filename = sys.argv[1]
-        with open(filename, 'r') as f:
-            grid = [list(map(int, line.split())) for line in f if line.strip()]
-    except IndexError:
-        grid = [list(map(int, input().split())) for _ in range(9)]
-    solve_sudoku(grid)
+sudoku_board_input = [list(map(int, line.split())) for line in sys.stdin if line.strip()]
+solve_sudoku(sudoku_board_input)
